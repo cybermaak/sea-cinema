b805d40b74c16d053a03ad6c7c4f66e82f645836
Mohammed Kamel <cyber.maak@gmail.com>
2014-02-07 20:55:32 -0800

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SeaCinema::Application.config.secret_key_base = 'e83b21ddbafac820a55f61328e9754b9331f4171a07d23620f9aad0762bfc902bfc0301a860ba110324eb91acd145b08c217a8b2cc87e460cdbda646ea5a362b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SeaCinema::Application.config.session_store :cookie_store, key: '_sea-cinema_session'
